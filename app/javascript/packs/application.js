827a9530a05a244f078da9d6f89fa14dade97851
yusuke-mizoguchi <zunmokou@gmail.com>
2022-03-28 22:20:01 +0900

    Fix:cocoonの修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,18 +2,16 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-require("jquery")
-require("@nathanvda/cocoon")
-require("src/cocoon")
+require("jquery");
+require("@nathanvda/cocoon");
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import "jquery"
-import "bootstrap"
-import "../stylesheets/application.scss"
-import "cocoon-js";
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+import "jquery";
+import "bootstrap";
+import "../stylesheets/application.scss";
 
 
 Rails.start()
