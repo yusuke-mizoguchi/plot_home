beb3ba4bd80d147b2e9ac590e16f23566e32b554
yusuke-mizoguchi <zunmokou@gmail.com>
2022-03-31 00:54:09 +0900

    kaminariのajaxを変更

--- app/controllers/novels_controller.rb
+++ app/controllers/novels_controller.rb
@@ -4,7 +4,7 @@ class NovelsController < ApplicationController
 
   def index
     @q = Novel.ransack(params[:q])
-    @novels = @q.result(distinct: true).includes(:user).order(created_at: :desc)
+    @novels = @q.result(distinct: true).includes(:user).order(created_at: :desc).page(params[:page])
   end
 
   def new
