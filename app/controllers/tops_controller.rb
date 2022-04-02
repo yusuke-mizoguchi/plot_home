3d98d1d8520c9d8661e0e9793200fb43f2562a62
yusuke-mizoguchi <zunmokou@gmail.com>
2022-04-02 19:30:42 +0900

    Add:初期データ投入他

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,6 +2,7 @@ class TopsController < ApplicationController
     skip_before_action :require_login
 
     def top
-        @reviews = Review.order(created_at: :desc).limit(1)
+        @reviews = Review.order(created_at: :desc).limit(3)
+        @novels = Novel.order(created_at: :desc).limit(5)
     end
 end
