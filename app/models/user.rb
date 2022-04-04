a5562fbb22ffbf84914d8d6cc1a2c1a0950b6cad
yusuke-mizoguchi <zunmokou@gmail.com>
2022-04-04 20:50:45 +0900

    Add:批評編集のajax化

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 4, maximum: 15 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :plofile, length: { maximum: 5000 }
+  validates :profile, length: { maximum: 5000 }
 
   validates :name, presence: true
   validates :email, uniqueness: true, presence: true
