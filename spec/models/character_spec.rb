ed835ff9ea1fe4859ac2bf21ca45c1a635174984
yusuke-mizoguchi <zunmokou@gmail.com>
2022-04-07 22:27:03 +0900

    Add:characterモデルスペック

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Character, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーション' do
+    it 'attributes' do
+      character = build(:character)
+      expect(character).to be_valid
+      expect(character.errors).to be_empty
+    end
+  end
 end
