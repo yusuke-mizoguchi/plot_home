032dd28de3a8f19903279a76d46a371430c4620a
yusuke-mizoguchi <zunmokou@gmail.com>
2022-04-22 16:32:57 +0900

    Fix:トップページの表示、返信ajax修正

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,7 +2,23 @@ class TopsController < ApplicationController
     skip_before_action :require_login
 
     def top
-        @reviews = Review.includes(:user).order(created_at: :desc).limit(3)
-        @novels = Novel.where.not(release: 'draft').includes(:user).order(created_at: :desc).limit(3)
+        @reviews = Review.select(:user_id).where(id: Review.select("DISTINCT ON (user_id) id").order(:user_id, created_at: :desc)).order(created_at: :desc).limit(3)
+        @novels_post = Novel.where.not(release: 'draft').order(created_at: :desc)
+
+        if current_user&.role == 'writer' && @novels_post.where.not(release: 'draft')
+            @narrow = @novels_post.where.not(release: 'draft')
+        elsif current_user&.role == 'reader' && top_narrow
+            @narrow = top_narrow
+        elsif @novels_post.where(release: 'release')
+            @narrow = @novels_post.where(release: 'release')
+        end
+
+        @novels = @narrow.limit(3)
+    end
+
+    private
+
+    def top_narrow
+        @novels_post.novels.where(release: 'reader').or(@user.novels.where(release: 'release'))
     end
 end
