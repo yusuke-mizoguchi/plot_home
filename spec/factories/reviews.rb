bc374400c7f5ed3aedb4e57ce624a542590b76d2
yusuke-mizoguchi <zunmokou@gmail.com>
2022-04-26 22:41:50 +0900

    Fix:reviews_spec修正/top修正

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :review do
     sequence(:good_point) { |n| "good-#{n}"}
     sequence(:bad_point) { |n| "bad-#{n}"}
-    sequence(:comment) { |n| "comment-#{n}" }
+    comment { "Review" }
     association :user
     association :novel
 
     trait :comment do
-      sequence(:good_point) { |n| "reply-#{n}" }
+      good_point { "Reply" }
       sequence(:comment) { |n| "comment-#{n}" }
     end
   end
